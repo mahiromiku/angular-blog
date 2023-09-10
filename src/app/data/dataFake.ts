4c1b9e3573c33211a0cf5c563930c196f873c831
Lucas <lucas25th@gmail.com>
2023-09-09 21:33:18 -0300

    projeto blog adaptado ao tema evangelion

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://i.pinimg.com/originals/83/82/aa/8382aa005af172690128f3439e3df0fd.jpg",
+    "title": "Sobre Shinji Ikari",
+    "description": "Shinji Ikari é o protagonista da série de anime e mangá Neon Genesis Evangelion, conhecido por sua complexidade psicológica e emoções intensas. Ele é um adolescente relutante que é convocado para pilotar um mecha gigante para combater ameaças alienígenas, enquanto lida com questões de autoestima, identidade e relacionamentos difíceis com seu pai e outros pilotos. A jornada de Shinji é marcada por um profundo conflito interno, tornando-o um personagem icônico na história da animação japonesa."
+  },
+  {
+    "id": "2",
+    "photoCover": "https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc-f:fbcover/wp-content/uploads/2020/08/legiao_oZNikgVRTqtU.png.webp",
+    "title": "Sobre Eva-01",
+    "description": "O Eva-01 é uma das unidades Evangelion na série Neon Genesis Evangelion. É pilotado por Shinji Ikari e é uma das máquinas de combate gigantes construídas para enfrentar ameaças misteriosas chamadas Anjos. O Eva-01 se destaca por sua aparência icônica, com cores distintas e um design biomecânico impressionante. Ao longo da série, o Eva-01 se torna central na trama, revelando segredos profundos sobre sua natureza e a conexão entre os personagens principais. Ele representa um elemento crucial na narrativa, misturando elementos de ficção científica, psicologia e simbolismo religioso."
+  },
+  {
+    "id": "3",
+    "photoCover": "https://i.pinimg.com/1200x/2d/01/a1/2d01a13a3537e6d2519e2c3239883371.jpg",
+    "title": "Sobre Asuka Langley Soryu",
+    "description": "Uma piloto alemã extremamente talentosa que pilota o Eva-02. Ela é conhecida por sua personalidade extrovertida, mas também carrega suas próprias inseguranças e problemas emocionais."
+  },
+  {
+    "id": "4",
+    "photoCover": "https://pm1.aminoapps.com/6646/47885f36017fd70d80653b9a1c18c15d804cdd1e_hq.jpg",
+    "title": "Sobre Rei Ayanami?",
+    "description": " Uma das pilotos de Evangelion, Rei é misteriosa e inicialmente carece de expressões emocionais. Ela é uma figura enigmática e parte central da trama."
+  },
+]
